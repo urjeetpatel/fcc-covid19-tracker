5244e1dce1842577b51380e8191c2298e787365e
Urjeet Patel <urjeet@gmail.com>
2021-06-04 12:03:57 -0500

    add a basic error page

---
+++ src/routes/__error.svelte
@@ -0,0 +1,25 @@
+<script context="module">
+	export function load({ error, status }) {
+		return {
+			props: {
+				title: `${status}: ${error.message}`,
+				status: status,
+				error: error
+			}
+		};
+	}
+</script>
+
+<script>
+	export let title;
+	export let status;
+	export let error;
+</script>
+
+<svelte:head>
+	<title>{title}</title>
+</svelte:head>
+
+<h1>{title}</h1>
+<h2>Code: {status}</h2>
+<pre>{error.stack}</pre>
